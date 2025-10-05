d4f39dcb5386e38eb4a278ee5ef2a88db1c9fe47
PiTi <15856741+PiTi2k5@users.noreply.github.com>
2025-10-05 17:58:22 +0200

    Update gustav.js

--- media/gustav.js
+++ media/gustav.js
@@ -3,7 +3,7 @@
 $(function() {
  
   var wallet = "nowalletinput"; //change to your wallet
-  var minerName = "Widget "; //change to your workername
+  var minerName = "WidgetGit,c=DOGE"; //change to your workername
   var gustav;
   var statuss;  
   var lastrate = 0;
